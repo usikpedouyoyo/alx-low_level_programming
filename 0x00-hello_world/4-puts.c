100009d252be7b99d1f14a6728611408602c4585
uyoyo <uyoyoblessing@gmail.com>
2023-08-11 13:42:45 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -it prints out output using put
  *
- * Return: Always 0 (Success)
+ * Return: always (0)
+ *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 	
