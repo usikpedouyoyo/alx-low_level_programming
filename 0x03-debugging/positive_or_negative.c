5f8eac699af038fd9be345f70fcb4c3ed63ea718
uyoyo <uyoyoblessing@gmail.com>
2023-08-19 11:17:41 -0700

    update

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Determine if a umber is positive, nnegative or zero.
+ * 0 : is the number to be checked
+ * Return: 0 on success
+ */
+void positive or negative(int i)
+{
+	(if i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}	
