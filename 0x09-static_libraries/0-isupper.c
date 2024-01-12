79c25e9a33fe42c6817a36644396ff6163288c71
uyoyo <uyoyoblessing@gmail.com>
2024-01-12 14:20:47 -0800

    libalkl

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _isupper(int c)
+{
+    return (c >= 'A' && c <= 'Z');
+}
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _isdigit(int c)
+{
+    return isdigit(c);
+}
+
+
